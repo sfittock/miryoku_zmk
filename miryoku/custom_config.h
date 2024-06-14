5973f23a6f2965d7a077be34d98445203cf4c897
sfittock <67087809+sfittock@users.noreply.github.com>
2024-06-15 00:19:57 +0800

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,2 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+lh_pht(LGUI, A),     lh_pht(LALT, R),     lh_pht(LCTRL, S),    lh_pht(LSHFT, T),    &kp G,             &kp M,             rh_pht(LSHFT, N),    rh_pht(LCTRL, E),    rh_pht(LALT, I),     rh_pht(LGUI, O),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
