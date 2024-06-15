2f28ed04abcf6243f2118336c5d1cedd6b7b044a
sfittock <67087809+sfittock@users.noreply.github.com>
2024-06-15 10:19:39 +0800

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,6 @@
 // https://github.com/manna-harbour/miryoku
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+LH_PHT(LGUI, A),     LH_PHT(LALT, R),     LH_PHT(LCTRL, S),    LH_PHT(LSHFT, T),    &kp G,             &kp M,             RH_PHT(LSHFT, N),    RH_PHT(LCTRL, E),    RH_PHT(LALT, I),     RH_PHT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_FUN, DEL),  U_LT(U_NUM, BSPC), U_LT(U_SYM, RET),  U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_MEDIA, ESC),U_NP,              U_NP
